7b361e296bd827396d87837bd44cb86175078da7
Enis Erkaya <enis.erkaya@intra.local>
2020-03-03 14:33:49 +0300

    kullanici sehir

---
+++ lib/model/kullanici.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter_app_ders1/model/sehir.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'kullanici.g.dart';
+
+@JsonSerializable()
+class Kullanici extends Object {
+ String ad;
+ String soyad;
+ List<Sehir> sehirList;
+
+ Kullanici(this.ad, this.soyad, this.sehirList);
+
+
+
+}
