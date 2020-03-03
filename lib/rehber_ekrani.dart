1c6591b81c09679ffe89940c38897af31e9dac2d
Enis Erkaya <enis.erkaya@intra.local>
2020-03-03 16:06:00 +0300

    izin alindi

--- lib/rehber_ekrani.dart
+++ lib/rehber_ekrani.dart
@@ -1,4 +1,6 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class RehberEkrani extends StatefulWidget {
   @override
@@ -6,8 +8,43 @@ class RehberEkrani extends StatefulWidget {
 }
 
 class _RehberEkraniState extends State<RehberEkrani> {
+  List _rehberList = new List();
+  @override
+  void initState() {
+    izinIste().then((izinVerildiMi){
+      if(izinVerildiMi){
+        rehberiGetir().then((rehber){
+          var rehberList = rehber.toList();
+          setState(() {
+            _rehberList = rehberList;
+          });
+        });
+      }
+
+    });
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+
+  Future<bool> izinIste() async {
+    var result = await PermissionHandler().requestPermissions([PermissionGroup.contacts]);
+    if(result[PermissionGroup.contacts] == PermissionStatus.granted){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+
+  Future<Iterable<Contact>>rehberiGetir() async {
+    Iterable<Contact> contacts = await ContactsService.getContacts();
+    return contacts;
+  }
+
+
 }
