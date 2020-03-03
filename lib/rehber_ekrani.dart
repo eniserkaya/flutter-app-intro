f2bf8515e4ba31bbc87d55c81cbaf98048c5f9c4
Enis Erkaya <enis.erkaya@intra.local>
2020-03-03 16:35:58 +0300

    izin alindi

--- lib/rehber_ekrani.dart
+++ lib/rehber_ekrani.dart
@@ -9,42 +9,62 @@ class RehberEkrani extends StatefulWidget {
 
 class _RehberEkraniState extends State<RehberEkrani> {
   List _rehberList = new List();
+
   @override
   void initState() {
-    izinIste().then((izinVerildiMi){
-      if(izinVerildiMi){
-        rehberiGetir().then((rehber){
+    izinIste().then((izinVerildiMi) {
+      if (izinVerildiMi) {
+        rehberiGetir().then((rehber) {
           var rehberList = rehber.toList();
           setState(() {
             _rehberList = rehberList;
           });
         });
       }
-
     });
 
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rehber'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: _rehberList.length > 0
+            ? ListView.builder(
+                itemCount: _rehberList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Contact contact = _rehberList.elementAt(index);
+                  return ListTile(
+                    leading: (contact.avatar != null && contact.avatar.length > 0) ? CircleAvatar(
+                      backgroundImage: MemoryImage(contact.avatar),
+                    ) : CircleAvatar(child: Text(contact.initials()),),
+                    title: Text(contact.displayName),
+                  );
+                })
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+    );
   }
 
   Future<bool> izinIste() async {
-    var result = await PermissionHandler().requestPermissions([PermissionGroup.contacts]);
-    if(result[PermissionGroup.contacts] == PermissionStatus.granted){
+    var result = await PermissionHandler()
+        .requestPermissions([PermissionGroup.contacts]);
+    if (result[PermissionGroup.contacts] == PermissionStatus.granted) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
   }
 
-
-  Future<Iterable<Contact>>rehberiGetir() async {
+  Future<Iterable<Contact>> rehberiGetir() async {
     Iterable<Contact> contacts = await ContactsService.getContacts();
     return contacts;
   }
-
-
 }
